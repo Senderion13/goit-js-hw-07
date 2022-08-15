b5da985483524d1914bc956f274246ba5967538a
Senderion13 <serovandrey2004@gmail.com>
2022-08-15 14:43:49 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+let gallery = document.querySelector(".gallery");
+let myModal;
+
+galleryItems.map((item) => {
+    gallery.innerHTML += `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img alt="${item.description}" data-source="${item.original}" src="${item.preview}" width="100%" class="gallery__image" /></a></div>`
+})
+
+let func = (e) => {
+    console.log("a");
+    if(e.code === "Escape"){
+        myModal.close();
+        gallery.removeEventListener("keydown", func)
+    }
+}
+
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+    myModal = basicLightbox.create(`
+        <div>
+            <img
+                src="${event.target.dataset.source}"
+                alt="${event.target.alt}"
+                height="700vh"
+            />
+        </div>
+    `)
+    myModal.show();
+    gallery.addEventListener("keydown", func);
+})
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+let gallery = document.querySelector(".gallery");
+
+galleryItems.map((item) => {
+    gallery.innerHTML += `<a class="gallery__item" href="${item.original}"><img captionsDelay="250ms" title="${item.description}" alt="${item.description}" data-source="${item.original}" src="${item.preview}" width="100%" class="gallery__image" /></a>`
+})
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+});
+lightbox.on('show.simplelightbox');
